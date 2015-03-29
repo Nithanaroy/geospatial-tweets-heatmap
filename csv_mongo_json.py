31872bd14be0b6ed6da6bc09378e36c0cc4df134
Nitin Pasumarthy <nithanaroy@gmail.com>
2015-03-29 12:58:44 -0700

    Added CSV to MongoDB transform script

---
+++ csv_mongo_json.py
@@ -0,0 +1,14 @@
+import re
+
+out1 = "sample_full.json"
+out2 = "full_text3.json"
+
+inp1 = "sample_full2.csv"
+inp2 = "full_text2.csv"
+
+with open(inp2, "r") as fr:
+	with open(out2, "w") as fw:
+		fr.next() # ignore the header line of csv
+		for line in fr:
+			temp = re.split(r',',line) # time, lat, long
+			fw.write('{time: "%s", loc: [%s, %s]}\n' % (temp[0].strip(), temp[2].strip(), temp[1].strip()))  # longitude, latitude
