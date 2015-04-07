b1446b89113bdbadda4e185ffcb63f4cf48da3f1
Nitin Pasumarthy <nithanaroy@gmail.com>
2015-04-07 11:31:12 -0700

    Fixed a typo

--- tweet_service.py
+++ tweet_service.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 try:
-    if os.environ['ENV'] == 'PR0DUCTION':
+    if os.environ['ENV'] == 'PRODUCTION':
         ENV = 'PRODUCTION'
     if os.environ['DEBUG'] == 'False':
         D = False
