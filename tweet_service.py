2efe86423f221a6c350475f9e3fdb1eea717862a
Nitin Pasumarthy <nithanaroy@gmail.com>
2015-04-08 13:43:45 -0700

    Computed counts at zoom 3, [Bug Fix]
    There was a bug in fetch_environment_variable function and this is fixed. The counts of tweets are computed but not yet saved to DB

--- tweet_service.py
+++ tweet_service.py
@@ -6,6 +6,7 @@ from pymongo import MongoClient
 import os
 from flask import Flask, request
 import re, time, json, pymongo
+from collections import namedtuple
 
 
 DB = 'tstream'
@@ -29,15 +30,18 @@ def get_profile_info():
     return cursor.next()['millis']
 
 
-def fetch_records(coords):
+def fetch_records(coords, only_count=False):
     collection = db.tweets_tail
     query = {"loc": {"$geoWithin": {"$box": coords}}}
     log(query)
     cursor = collection.find(query, {"loc": 1, "_id": 0})
     start = time.clock()
-    tweets = []
-    for tweet in cursor:
-        tweets.append(json.dumps(tweet))
+    if (only_count):
+        tweets = cursor.count()
+    else:
+        tweets = []
+        for tweet in cursor:
+            tweets.append(json.dumps(tweet))
     end = time.clock() - start
     log('Completed fetching tweets', True)
     return {"tweets": tweets, "time": end}
@@ -93,20 +97,54 @@ def log(message, only_debug=False):
 def fetch_environment_variable(key, default=None):
     value = default
     try:
-        value = os.environ[key] == 'PRODUCTION'
+        value = os.environ[key]
     finally:
         return value
 
 
-def create_viz_index():
-    pass
+def create_viz_index(win):
+    """
+    Creates a visualization index for the given window. The index is an aggreagation of tweets for each zoom level
+    :param win: Southwest and Northeast co-ordinates of the query window.
+    It is a dict type in this format {"sw": (long,lat), "ne": (long,lat)}
+    :return: true if the index is successfully created, else false
+    """
+    point = namedtuple('point', 'long lat')
+    box = namedtuple('box', 'sw ne nw se')
+    ne = point(float(win["ne"][0]), float(win["ne"][1]))
+    sw = point(float(win["sw"][0]), float(win['sw'][1]))
+    nw = point(float(win['sw'][0]), float(win['ne'][1]))  # long from SW and lat from NE
+    se = point(float(win['ne'][0]), float(win['sw'][1]))  # long from NE and lat from SW
+    current_zoom = 3  # valid range [0 to 19]. 0 is the entire world
+    longpieces = 8  # number of partitions at this zoom level along a latitude
+    latpieces = 6  # number of partitions at this zoom level along a longitude
+    longstep = abs(ne.long - nw.long) / longpieces
+    latstep = abs(ne.lat - se.lat) / latpieces
+
+    res = []
+
+    for i in range(1, latpieces + 1):
+        for j in range(1, longpieces + 1):
+            current_win = box(point(nw.long, nw.lat - latstep), point(nw.long + longstep, nw.lat), nw,
+                              point(nw.long + longstep, nw.lat - latstep))
+            dbrecords = fetch_records(
+                [[current_win.sw.long, current_win.sw.lat], [current_win.ne.long, current_win.ne.lat]], True)
+            res.append(
+                {'point': {'long': nw.long + longstep / 2, 'lat': nw.lat - latstep / 2},
+                 'tweets': dbrecords['tweets'],
+                 'time': dbrecords['time']})
+            nw = point(nw.long + longstep, nw.lat)
+        nw = point(nw.long - longpieces * longstep, nw.lat - latstep)
+        log("i = {0}, j = {1}".format(i, j))
+    return res
 
 
 @app.route('/rect', methods=['GET', 'POST'])
 def rect():
     log('Received req: {0}'.format(request.form))
     f = request.form
-    res = fetch_records([[float(f.get("ALong")), float(f.get("ALat"))], [float(f.get("BLong")), float(f.get("BLat"))]])
+    # res = fetch_records([[float(f.get("ALong")), float(f.get("ALat"))], [float(f.get("BLong")), float(f.get("BLat"))]])
+    res = create_viz_index({"sw": (-124, 27), "ne": (-59, 48)})
     return jsonify({"tweets": res['tweets'],
                     "apptime": res['time'],
                     "dbtime": get_profile_info()})
@@ -138,7 +176,7 @@ else:
 db = client.tstream  # tstream is the name of the collection
 db.set_profiling_level(pymongo.ALL)
 
-log('Running in {0} ENVIRONMENT'.format(ENV))
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    log('Running in {0} ENVIRONMENT'.format(ENV))
+    # app.run(debug=True, host='0.0.0.0')
+    create_viz_index({"sw": (-124, 27), "ne": (-59, 48)})
